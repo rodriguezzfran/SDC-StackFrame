e97c22b9ff99a946e85c885b8459b68f1b3517ab
Mauricio L Valdez Benavidez <103122420+mauriciovaldez19@users.noreply.github.com>
2024-04-07 17:51:33 -0300

    Actualizacion
    Se modifican algunos colores de la ventana, pero también se agrega la posibilidad de seleccionar varios países

--- GINI_interface.py
+++ GINI_interface.py
@@ -2,7 +2,7 @@ import requests #Importamos la librería requests para hacer solicitudes HTTP
 import json #Importamos la librería json para trabajar con archivos JSON
 import ctypes #Importamos la librería ctypes que nos deja mandar la informacion a el programa en c
 import numpy as np #Importamos la librería numpy para trabajar con arreglos de datos
-from tkinter import Tk, Label, Button #Importamos la librería tkinter para crear una interfaz gráfica
+from tkinter import Tk, Label, Button,ttk #Importamos la librería tkinter para crear una interfaz gráfica
 
 # URL de la API a dónde vamos a hacer las solicitudes 
 URL = 'https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=%22Argentina%22'
@@ -53,10 +53,13 @@ def update_label(result,label):
     label['text'] = f'El resultado del indice GINI es: {result}'
 
 #funcion para crear la interfaz grafica
-def on_button_click(label):
+def on_button_click(label, country_code):
+    if not country_code:
+        label.config(text="Seleccionar un país")
+        return
     data = get_data() #Obtenemos la data de la API
     if data: #Si la data no es None
-        filtered_data = filter_country(data[1], 'AR')
+        filtered_data = filter_country(data[1], country_code)
         values = filter_values(filtered_data)
         values = np.array(values, dtype=np.float32)
         result = calculator.average_plus_one(values.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), len(values))
@@ -67,13 +70,21 @@ def on_button_click(label):
 def create_gui():
     root = Tk() #Creamos una ventana
     root.title('Calculadora GINI') #Le ponemos un título a la ventana
+   # Cambiando el color de fondo de la ventana
+    root.configure(bg='#f0f0f0')
     
     # Etiqueta que mostrará el resultado
-    result_label = Label(root, text="Solicitar datos para obtener el índice GINI de Argentina")
+    result_label = Label(root, text="Click para obtener el índice GINI del país seleccionado", fg='#333', font=('Helvetica', 14), bg='#f0f0f0')
     result_label.pack()
 
+ # Combobox para seleccionar el país
+    country_label = Label(root, text="Seleccionar país:", bg='#f0f0f0')
+    country_label.pack()
+    country_combobox = ttk.Combobox(root, values=["AR", "BR", "CL", "UY","PY","US","IT","ES"])  # Se pueden agregar más códigos de paises
+    country_combobox.pack()
+
     # Botón que al presionarlo actualizará la etiqueta
-    request_button = Button(root, text="Solicitar Datos", command=lambda: on_button_click(result_label))
+    request_button = Button(root, text="Solicitar Datos", command=lambda: on_button_click(result_label,country_combobox.get()), bg='#007bff', fg='white', font=('Helvetica', 12))
     request_button.pack()
 
     root.mainloop() #Iniciamos el loop de la ventana
